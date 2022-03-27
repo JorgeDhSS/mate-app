a9adef88104339f07fdf8c4189188a48540301d6
Jorge domínguez <herrerajorge725@gmail.com>
2022-03-27 01:32:51 -0600

    model y migrate de actividad

---
+++ app/Actividad.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Actividad extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'descripcion', 'titulo', 'fechaInicio', 'fechaCierre', 'asesor_id'
+    ];
+    public function asesor()
+    {
+        return $this->hasOne(Asesor::class);
+    }
+}
---
+++ database/migrations/2022_03_27_071538_create_actividads_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActividadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actividads', function (Blueprint $table) {
+            $table->id();
+            $table->string('descripcion');
+            $table->string('titulo');
+            $table->date('fechaInicio');
+            $table->date('fechaCierre');
+            $table->foreignId('asesor_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actividads');
+    }
+}

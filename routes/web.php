2456d72b4a63781ba3e246698643b55ce956da41
Yielcy <jonthan.galio108@gmail.com>
2022-03-29 16:43:28 -0600

    Archivo AsesorController y ruta de la vista de groupPract

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\registroController;
 use App\Http\Controllers\sesionController;
+use App\Http\Controllers\AsesorController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,3 +19,4 @@ Route::get('/', function () {
     return view('home');
 });
 Route::get('director/addAsesor','DirectorController@createAsesorView')->name('director.addAsesor');
+Route::get('asesor/groupPract', 'AsesorController@groupPractView')->name('asesor.groupPract');

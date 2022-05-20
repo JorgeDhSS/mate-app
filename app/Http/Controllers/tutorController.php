512d0d376bb39baa2137e63d9f36135dd23dd177
JAngelGtzRguez <angel.rogu017@gmail.com>
2022-05-20 16:28:06 -0500

    Vista y funcion generar reporte

--- app/Http/Controllers/tutorController.php
+++ app/Http/Controllers/tutorController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Practicante;
-use App\Respuesta_Pregunta;
 use App\Tutor;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+//use PDF;
+//use Barryvdh\DomPDF\PDF;
+use Barryvdh\DomPDF\Facade as PDF;
+
 
 class TutorController extends Controller{
+
+    public function imprimir(){
+        $details =['title' => 'test'];
+        $pdf = PDF::loadView('tutor_views.generarReporte', $details);
+        return $pdf->download('ReporteActividades.pdf');
+   }
+
     public function generarReporteView(){
         return view('tutor_views.generarReporte');
     }
@@ -16,13 +26,47 @@ class TutorController extends Controller{
 
     public function getPuntuajes()
     {
-        $Tutor = Tutor::where('user_id', Auth::user()->id)->first();
-        $respuestas = Practicante::where('tutor_id', '=', $Tutor->id)
-                        ->leftJoin('respuesta__preguntas', 'respuesta__preguntas.practicante_id', '=', 'practicantes.id')
-                        ->has('respuesta_pregunta')
-                        ->select("practicantes.*", DB::raw('SUM(respuesta__preguntas.correcta) as correctas'), DB::raw('count(respuesta__preguntas.correcta) as total'))
-                        ->groupBy('practicantes.id')
-                        ->get();
-        return view('tutor_views.generarReporte', ["respuestas" => $respuestas]);
+        // if(Auth::user() != null){
+        //     $Tutor = Tutor::where('user_id', Auth::user()->id)->first();
+        //     $respuestas = Practicante::where('tutor_id', '=', $Tutor->id)
+        //                     ->leftJoin('respuesta__preguntas', 'respuesta__preguntas.practicante_id', '=', 'practicantes.id')
+        //                     ->has('respuesta_pregunta')
+        //                     ->select("practicantes.*", DB::raw('SUM(respuesta__preguntas.correcta) as correctas'), DB::raw('count(respuesta__preguntas.correcta) as total'))
+        //                     ->groupBy('practicantes.id')
+        //                     ->get();
+        //     return view('tutor_views.generarReporte', ["respuestas" => $respuestas]);
+        // }else{
+        //     return view('tutor_views.generarReporte')->with(['alert' => "Swal({
+        //         title: 'No has iniciado sesión!',
+        //         text: 'Inicia sesion para continuar',
+        //         icon: 'error',
+        //         showCancelButton: 'false', 
+        //         showConfirmButton: 'false'
+        //     });"]);
+        // }
+
+
+
+
+
+        if(Auth::user() != null){
+            $Tutor = Tutor::where('user_id', 55)->first();
+            $respuestas = Practicante::where('tutor_id', '=', $Tutor->id)
+                            ->leftJoin('respuesta__preguntas', 'respuesta__preguntas.practicante_id', '=', 'practicantes.id')
+                            ->has('respuesta_pregunta')
+                            ->select("practicantes.*", DB::raw('SUM(respuesta__preguntas.correcta) as correctas'), DB::raw('count(respuesta__preguntas.correcta) as total'))
+                            ->groupBy('practicantes.id')
+                            ->get();
+            return view('tutor_views.generarReporte', ["respuestas" => $respuestas]);
+        }else{
+            return view('tutor_views.generarReporte')->with(['alert' => "Swal({
+                title: 'No has iniciado sesión!',
+                text: 'Inicia sesion para continuar',
+                icon: 'error',
+                showCancelButton: 'false', 
+                showConfirmButton: 'false'
+            });"]);
+        }
+        
     }
 }
--- resources/views/tutor_views/generarReporte.blade.php
+++ resources/views/tutor_views/generarReporte.blade.php
@@ -20,9 +20,9 @@
         <div class="w-2/6 p-2  text-center">
             <div class="flex mb-4">
                 <div class="w-1/2 p-2  text-center">
-                    <button class="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-6 border-b-4 border-green-700 hover:border-green-500 rounded">
+                    <a href="{{route('tutor.generarReporte')}}" class="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-6 border-b-4 border-green-700 hover:border-green-500 rounded">
                     Imprimir
-                    </button>
+                    </a>
                 </div>
                 <div class="w-1/2 p-2 text-center">
                     <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
@@ -34,8 +34,44 @@
     </div>
 
     <!--MOSTRAR PRACTICANTES-->
-    <div>
-
-    </div>
+    @foreach($respuestas as $r)
+        <div class="w-full rounded overflow-hidden shadow-lg">
+            <div class="w-full p-2 bg-gray-400 text-center"><div class="font-bold text-xl mb-2 text-center">{{$r->user->name}}</div></div>
+            <div class="flex mb-4">
+                <div class="w-1/2 p-2 bg-gray-400 text-center">
+                    <div class="px-6 py-4">
+                        <p class="">
+                            <label >Matricula: </label>{{$r->matricula}}
+                            <br>
+                            <br>
+                            <label >Nivel Escolar: </label>{{$r->nivelEscolar}}
+                        </p>
+                    </div>
+                </div>
+                <div class="w-1/2 p-2 bg-gray-500 text-center">
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2 text-center"></div>
+                            <div class="flex mb-4">
+                                <div class="w-1/2 p-2 bg-gray-400 text-center">
+                                    <p class="">
+                                        <label class="text-center">Aciertos</label>
+                                        <br>
+                                        <input class="border border-green-500 text-center h-20 w-20" type="text" value="{{$r->correctas}}">
+                                    </p>
+                                </div>
+                                <div class="w-1/2 p-2 bg-gray-500 text-center">
+                                    <p class="">
+                                        <label class="text-center">Total de puntos</label>
+                                        <br>
+                                        <input class="border border-green-500 text-center h-20 w-20" type="text" value="{{$r->total}}">
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+        </div>
+    @endforeach
 
 @endsection
